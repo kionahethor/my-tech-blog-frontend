e78675401af04eb585fbcfdf794e5cdc21e1c367
Kiona Hethor <hethorkiona@gmail.com>
2026-05-10 21:23:51 +0200

    added SEO meta tags for better browser formatting

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup>
+useHead({
+  title: 'My Tech Blog',
+  meta: [
+    { name: 'description', content: 'A full-stack blog built with Nuxt and Strapi.' }
+  ]
+})
+</script>
+
 <template>
   <div>
     <NuxtLayout>
